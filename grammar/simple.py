321426e4d284a8a729b18531733a3881b94cfb09
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2017-02-08 16:51:59 +0100

    Add basic support for attack relations in the simple grammar.

--- grammar/simple.py
+++ grammar/simple.py
@@ -12,6 +12,12 @@ def support(claim, specifics, general=None):
     return argument
 
 
+def attack(claim, specifics):
+    relation = Relation(specifics, claim, Relation.ATTACK)
+    argument = Argument(relations={relation})
+    return argument
+
+
 grammar = and_rules('SPECIFIC_CLAIMS', 'SPECIFIC_CLAIM') \
     | {
         Rule('ARGUMENT', [RuleRef('SENTENCE')],
@@ -24,12 +30,23 @@ grammar = and_rules('SPECIFIC_CLAIMS', 'SPECIFIC_CLAIM') \
         Rule('SENTENCE', [RuleRef('SUPPORTED_CLAIM'), Literal('.')],
             lambda state, data: data[0]),
 
+        Rule('SENTENCE', [RuleRef('ATTACKED_CLAIM'), Literal('.')],
+            lambda state, data: data[0]),
+
 
         Rule('SUPPORTED_CLAIM', [RuleRef('SPECIFIC_CLAIM'), Literal('because'), RuleRef('SPECIFIC_CLAIM')],
             lambda state, data: data[0] + data[2] + Interpretation(argument=support(data[0].local, specifics={data[2].local}))),
 
         Rule('SUPPORTED_CLAIM', [RuleRef('SPECIFIC_CLAIM'), Literal('because'), RuleRef('SPECIFIC_CLAIMS')],
             lambda state, data: data[0] + data[2] + Interpretation(argument=support(data[0].local, specifics=data[2].local))),
+
+
+        Rule('ATTACKED_CLAIM', [RuleRef('SPECIFIC_CLAIM'), Literal('but'), RuleRef('SPECIFIC_CLAIM')],
+            lambda state, data: data[0] + data[2] + Interpretation(argument=attack(data[0].local, specifics={data[2].local}))),
+
+        Rule('ATTACKED_CLAIM', [RuleRef('SPECIFIC_CLAIM'), Literal('but'), RuleRef('SPECIFIC_CLAIMS')],
+            lambda state, data: data[0] + data[2] + Interpretation(argument=attack(data[0].local, specifics=data[2].local))),
+
 
         Rule('SUPPORTED_CLAIM', [RuleRef('SPECIFIC_CLAIM'), Literal('because'), RuleRef('GENERAL_CLAIM'), Literal('and'), RuleRef('SPECIFIC_CLAIM')],
             lambda state, data: data[0] + data[2] + data[4] + Interpretation(argument=support(data[0].local, general=data[2].local, specifics={data[4].local}))),
