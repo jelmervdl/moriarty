d98c92a8425e4f3d0fb06d84729d369cdaca9780
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2017-03-02 16:41:19 +0100

    Rewrite the way relations are added for because and but in preparation for general and specific claims handling

--- grammar/recursive.py
+++ grammar/recursive.py
@@ -4,31 +4,41 @@ from interpretation import Interpretation
 from grammar.macros import and_rules
 
 
-def _relation(relation_type, claim, specifics, general=None):
-    relation = Relation(specifics, claim, relation_type)
-    argument = Argument(relations={relation})
-    if general is not None:
-        argument = argument | Argument(relations={Relation([general], relation, relation_type)})
-    return argument
+class PartialRelation(object):
+    def __init__(self, relation_type, specifics, general=None):
+        assert all(o.__class__.__name__ == 'SpecificClaim' for o in specifics)
+        assert general is None or general.__class__.__name__ == 'GeneralClaim'
+        self.type = relation_type
+        self.specifics = specifics
+        self.general = general
 
+    def instantiate(self, claim):
+        assert claim.__class__.__name__ == 'SpecificClaim'
+        relation = Relation(sources=self.specifics, target=claim, type=self.type)
+        argument = Argument(relations={relation})
+        if self.general is not None:
+            argument = argument | Argument(relations={Relation([self.general], relation, self.type)})
+        return Interpretation(argument=argument, local=claim)
+        
 
 def expanded_claim(state, data):
     # specific claim; supports; attacks;
-    interpretation = data[0] + data[1] + data[2]
+    interpretation = data[0]
 
     # Add the supporting relations (if there is a 'because' clause)
     if data[1].local:
-        interpretation += Interpretation(argument=_relation(Relation.SUPPORT, data[0].local, specifics=data[1].local))
+        interpretation += data[1] + data[1].local.instantiate(data[0].local)
 
     # Add the attacking relations (if there is a 'but' clause)
     if data[2].local:
-        interpretation += Interpretation(argument=_relation(Relation.ATTACK, data[0].local, specifics=data[2].local))
+        interpretation += data[2] + data[2].local.instantiate(data[0].local)
 
-    # and don't forget to set local back to the specific claim we're augmenting.
-    return interpretation + Interpretation(local=data[0].local)
+    return interpretation
 
 
 grammar = and_rules('EXPANDED_CLAIMS', 'EXPANDED_CLAIM', accept_singular=True) \
+    | and_rules('EXPANDED_CLAIMS_GF', 'EXPANDED_CLAIM', first_singleton='GENERAL_CLAIM') \
+    | and_rules('EXPANDED_CLAIMS_GL', 'EXPANDED_CLAIM', last_singleton='GENERAL_CLAIM') \
     | {
         Rule('ARGUMENT', [RuleRef('SENTENCE')],
             lambda state, data: data[0]),
@@ -44,14 +54,14 @@ grammar = and_rules('EXPANDED_CLAIMS', 'EXPANDED_CLAIM', accept_singular=True) \
             expanded_claim),
 
         Rule('SUPPORT', [Literal('because'), RuleRef('EXPANDED_CLAIMS')],
-            lambda state, data: data[1] + Interpretation(local=data[1].local)),
+            lambda state, data: data[1] + Interpretation(local=PartialRelation(Relation.SUPPORT, specifics=data[1].local))),
 
         Rule('SUPPORT', [],
-            lambda state, data: Interpretation(local={})),
+            lambda state, data: Interpretation()),
 
         Rule('ATTACK', [Literal('but'), RuleRef('EXPANDED_CLAIMS')],
-            lambda state, data: data[1] + Interpretation(local=data[1].local)),
+            lambda state, data: data[1] + Interpretation(local=PartialRelation(Relation.ATTACK, specifics=data[1].local))),
 
         Rule('ATTACK', [],
-            lambda state, data: Interpretation(local={})),
+            lambda state, data: Interpretation()),
     }
