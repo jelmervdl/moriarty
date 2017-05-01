fb75a2c99e79de92747284264747e3c75ec88c4c
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2017-05-01 15:55:21 +0200

    Continue renaming conditional to general where appropriate.

--- grammar/recursive.py
+++ grammar/recursive.py
@@ -101,8 +101,8 @@ def expanded_claim(state, data):
 
 
 grammar = and_rules('EXPANDED_SPECIFIC_CLAIMS', 'EXPANDED_SPECIFIC_CLAIM', accept_singular=True) \
-    | and_rules('EXPANDED_SPECIFIC_CLAIMS_CONDITIONAL_FIRST', 'EXPANDED_SPECIFIC_CLAIM', first_singleton='EXPANDED_CONDITIONAL_CLAIM') \
-    | and_rules('EXPANDED_SPECIFIC_CLAIMS_CONDITIONAL_LAST', 'EXPANDED_SPECIFIC_CLAIM', last_singleton='EXPANDED_CONDITIONAL_CLAIM') \
+    | and_rules('EXPANDED_SPECIFIC_CLAIMS_GENERAL_FIRST', 'EXPANDED_SPECIFIC_CLAIM', first_singleton='EXPANDED_GENERAL_CLAIM') \
+    | and_rules('EXPANDED_SPECIFIC_CLAIMS_GENERAL_LAST', 'EXPANDED_SPECIFIC_CLAIM', last_singleton='EXPANDED_GENERAL_CLAIM') \
     | and_rules('SUPPORTS', 'SUPPORT', accept_singular=True) \
     | and_rules('ATTACKS', 'ATTACK', accept_singular=True) \
     | {
@@ -119,19 +119,24 @@ grammar = and_rules('EXPANDED_SPECIFIC_CLAIMS', 'EXPANDED_SPECIFIC_CLAIM', accep
         Rule('EXPANDED_SPECIFIC_CLAIM', [RuleRef('SPECIFIC_CLAIM'), RuleRef('SUPPORTS'), RuleRef('ATTACKS')],
             expanded_claim),
 
-        Rule('EXPANDED_CONDITIONAL_CLAIM', [RuleRef('CONDITIONAL_CLAIM'), RuleRef('SUPPORTS'), RuleRef('ATTACKS')],
+        # We use CONDITIONAL_CLAIM instead of GENERAL_CLAIM here because a conditional claim is a more specific
+        # structure that can contain a general claim, and that is why it has to be above general claim in the
+        # hierarchy. And hey, in a sense a general claim is also a conditional claim because for a general claim
+        # to be applicable the subject has to match the group of the subject of the general claim, making it
+        # more or less a conditional claim, right? 
+        Rule('EXPANDED_GENERAL_CLAIM', [RuleRef('CONDITIONAL_CLAIM'), RuleRef('SUPPORTS'), RuleRef('ATTACKS')],
             expanded_claim),
 
         Rule('SUPPORT', [Literal('because'), RuleRef('EXPANDED_SPECIFIC_CLAIMS')],
             lambda state, data: data[1] + Interpretation(local=PartialRelation(Relation.SUPPORT, specifics=data[1].local))),
 
-        Rule('SUPPORT', [Literal('because'), RuleRef('EXPANDED_SPECIFIC_CLAIMS_CONDITIONAL_FIRST')],
+        Rule('SUPPORT', [Literal('because'), RuleRef('EXPANDED_SPECIFIC_CLAIMS_GENERAL_FIRST')],
             lambda state, data: data[1] + Interpretation(local=PartialRelation(Relation.SUPPORT, conditional=data[1].local[0], specifics=data[1].local[1:]))),
 
-        Rule('SUPPORT', [Literal('because'), RuleRef('EXPANDED_SPECIFIC_CLAIMS_CONDITIONAL_LAST')],
+        Rule('SUPPORT', [Literal('because'), RuleRef('EXPANDED_SPECIFIC_CLAIMS_GENERAL_LAST')],
             lambda state, data: data[1] + Interpretation(local=PartialRelation(Relation.SUPPORT, conditional=data[1].local[-1], specifics=data[1].local[0:-1]))),
 
-        Rule('SUPPORT', [Literal('because'), RuleRef('EXPANDED_CONDITIONAL_CLAIM')],
+        Rule('SUPPORT', [Literal('because'), RuleRef('EXPANDED_GENERAL_CLAIM')],
             lambda state, data: data[1] + Interpretation(local=PartialRelation(Relation.SUPPORT, conditional=data[1].local))),
 
         Rule('SUPPORTS', [],
@@ -140,13 +145,13 @@ grammar = and_rules('EXPANDED_SPECIFIC_CLAIMS', 'EXPANDED_SPECIFIC_CLAIM', accep
         Rule('ATTACK', [Literal('but'), RuleRef('EXPANDED_SPECIFIC_CLAIMS')],
             lambda state, data: data[1] + Interpretation(local=PartialRelation(Relation.ATTACK, specifics=data[1].local))),
 
-        Rule('ATTACK', [Literal('but'), RuleRef('EXPANDED_SPECIFIC_CLAIMS_CONDITIONAL_FIRST')],
+        Rule('ATTACK', [Literal('but'), RuleRef('EXPANDED_SPECIFIC_CLAIMS_GENERAL_FIRST')],
             lambda state, data: data[1] + Interpretation(local=PartialRelation(Relation.ATTACK, conditional=data[1].local[0], specifics=data[1].local[1:]))),
 
-        Rule('ATTACK', [Literal('but'), RuleRef('EXPANDED_SPECIFIC_CLAIMS_CONDITIONAL_LAST')],
+        Rule('ATTACK', [Literal('but'), RuleRef('EXPANDED_SPECIFIC_CLAIMS_GENERAL_LAST')],
             lambda state, data: data[1] + Interpretation(local=PartialRelation(Relation.ATTACK, conditional=data[1].local[-1], specifics=data[1].local[0:-1]))),
 
-        Rule('ATTACK', [Literal('but'), RuleRef('EXPANDED_CONDITIONAL_CLAIM')],
+        Rule('ATTACK', [Literal('but'), RuleRef('EXPANDED_GENERAL_CLAIM')],
             lambda state, data: data[1] + Interpretation(local=PartialRelation(Relation.ATTACK, conditional=data[1].local))),
 
         Rule('ATTACKS', [],
