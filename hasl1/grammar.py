aa844b238681898bc0259ab87ae2810d9a7bca28
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2018-04-22 20:07:28 +0200

    Don't fuss about capitals (mainly at the start of sentences)

--- hasl1/grammar.py
+++ hasl1/grammar.py
@@ -325,7 +325,7 @@ class Claim(object):
         self.assumed = assumed
 
     def __eq__(self, other):
-        return str(self) == str(other)
+        return str(self).lower() == str(other).lower()
 
     def __str__(self):
         return "{subj} {verb} {neg}{obj}".format(neg="not " if self.negated else "", **self.__dict__)
